ec0f3480e9a8ee64dde6445e546fab2ad2269482
YeoungJun <dudwnsv123@naver.com>
2024-01-24 17:24:42 +0900

    17장 컬렉션

---
+++ src/ch17_collcetion/part1_list/sec03_Message/MessageServiceListImp1.java
@@ -0,0 +1,49 @@
+package ch17.collcetion.part1_list.sec03_Message;
+
+import java.util.List;
+
+public class MessageServiceListImp1 implements MessageService {
+
+	@Override
+	public Message findByMid(int mid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getMessageListAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getMessageListByWriter(String writer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMessage(int mid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/ch17_collcetion/part1_list/sec05_Member/Member.java
@@ -0,0 +1,47 @@
+package ch17.collcetion.part1_list.sec05_Member;
+
+public class Member {
+	private int age;
+	private String name;
+	private String gender;		// "남", "여"
+	
+	public Member() { }
+	public Member(int age, String name, String gender) {
+		this.age = age;
+		this.name = name;
+		this.gender = gender;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member m = (Member) obj;
+			return this.name.equals(m.getName());
+		}
+		return false;
+	}
+	@Override
+	public String toString() {
+		return "Member [age=" + age + ", name=" + name + ", gender=" + gender + "]";
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
+
