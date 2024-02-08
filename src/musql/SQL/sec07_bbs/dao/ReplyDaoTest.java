53b81e7bec852e85e9b62f61a4ab03c3005b7fb4
YeoungJun <dudwnsv123@naver.com>
2024-02-08 16:11:57 +0900

    ReplyDaoTest

---
+++ src/musql/SQL/sec07_bbs/dao/ReplyDaoTest.java
@@ -0,0 +1,26 @@
+package mysql.sec07_bbs.dao;
+
+import java.util.List;
+
+import mysql.sec07_bbs.entity.Reply;
+
+public class ReplyDaoTest {
+
+	public static void main(String[] args) {
+		ReplyDao rDao = new ReplyDao();
+		BoardDao bDao = new BoardDao();
+		String sessionUid = "sarah";
+		int bid = 12;
+		
+		Reply r = new Reply();
+		r.setComment("댓글 3"); r.setUid(sessionUid); r.setBid(bid);
+		rDao.insertReply(r);
+		bDao.increaseCount("reply", bid);
+		
+		List<Reply> list = rDao.getReplyList(bid);
+		list.forEach(x -> System.out.println(x));
+
+		bDao.close(); rDao.close();
+	}
+
+}
