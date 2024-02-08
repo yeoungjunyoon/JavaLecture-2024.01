06204d0a344f4314303cfc820ab2ded26c828371
YeoungJun <dudwnsv123@naver.com>
2024-02-08 16:11:17 +0900

    appl

---
+++ src/musql/SQL/sec07_bbs/appl/BoardAppl.java
@@ -0,0 +1,26 @@
+package mysql.sec07_bbs.appl;
+
+import java.util.List;
+
+import mysql.sec07_bbs.entity.Board;
+import mysql.sec07_bbs.service.BoardService;
+import mysql.sec07_bbs.service.BoardServiceMySQLImpl;
+
+public class BoardAppl {
+
+	public static void main(String[] args) {
+		BoardService bSvc = new BoardServiceMySQLImpl();
+		
+		List<Board> list = bSvc.getBoardList(1, "", "");
+		list.forEach(x -> System.out.println(x.listForm()));
+		System.out.println("=============================================");
+		
+		Board board = bSvc.getBoard(15);
+		System.out.println(board);
+		if (board.getReplyList() != null)
+			board.getReplyList().forEach(x -> System.out.println(x));
+		
+		bSvc.close();
+	}
+
+}
