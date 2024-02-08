a2220f24c0b2b9677c14d4bff684ac8db7dc132e
YeoungJun <dudwnsv123@naver.com>
2024-02-08 16:15:35 +0900

    sec07_bbs

---
+++ src/musql/SQL/sec07_bbs/dao/ReplyDao.java
@@ -0,0 +1,86 @@
+package mysql.sec07_bbs.dao;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import mysql.sec07_bbs.entity.Reply;
+
+public class ReplyDao {
+	private String connStr;
+	private String user;
+	private String password;
+	private Connection conn;
+	
+	public ReplyDao() {
+		String path = "C:/Workspace/Java/lesson/src/mysql/sec07_bbs/mysql.properties";
+		try {
+			Properties prop = new Properties();
+			prop.load(new FileInputStream(path));
+			
+			String host = prop.getProperty("host");
+			String port = prop.getProperty("port");
+			String database = prop.getProperty("database");
+			this.connStr = "jdbc:mysql://" + host + ":" + port + "/" + database;
+			this.user = prop.getProperty("user");
+			this.password = prop.getProperty("password");
+			this.conn = DriverManager.getConnection(connStr, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Reply> getReplyList(int bid) {
+		String sql = "SELECT r.*, u.uname FROM reply r"
+					+ "	JOIN users u ON r.uid=u.uid"
+					+ "	WHERE r.bid=?"
+					+ "	ORDER BY rid";
+		List<Reply> list = new ArrayList<Reply>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bid);
+			
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Reply r = new Reply(rs.getInt(1), rs.getString(2),
+							LocalDateTime.parse(rs.getString(3).replace(" ", "T")),
+							rs.getString(4), rs.getInt(5), rs.getString(6));
+				list.add(r);
+			}
+			rs.close(); pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public void insertReply(Reply reply) {
+		String sql = "insert into reply values (default, ?, default, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, reply.getComment());
+			pstmt.setString(2, reply.getUid());
+			pstmt.setInt(3, reply.getBid());
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/musql/SQL/sec07_bbs/service/BoardService.java
@@ -0,0 +1,22 @@
+package mysql.sec07_bbs.service;
+
+import java.util.List;
+
+import mysql.sec07_bbs.entity.Board;
+
+public interface BoardService {
+	public static final int COUNT_PER_PAGE = 10;
+
+	List<Board> getBoardList(int page, String field, String query);
+	
+	Board getBoard(int bid);
+	
+	void insertBoard(Board board);
+	
+	void updateBoard(Board board);
+	
+	void deleteBoard(int bid);
+	
+	void close();
+	
+}
